c74f4cd4255970056898c7e3e2a01a94fcd2ae41
Tony Baker <this.tonybaker@gmail.com>
2019-02-21 04:26:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,8 +73,12 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  output_array = []
   holiday_hash.each do |season_key, holiday_hash|
     holiday_hash.collect do |holiday_key, supply_array|
+      if supply_array.include?("BBQ")
+        output_array.push(holiday_key)
     end
   end
+  output_array
 end
