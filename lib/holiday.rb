653c215b4d023061608f1b51dc236322dc313a5a
Tony Baker <this.tonybaker@gmail.com>
2019-02-21 04:08:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday_hash_value.each do |holiday_key, supply_array_value|
       holiday_string_array = holiday_key.to_s.split('_')
       capital_holiday_string_array = holiday_string_array.collect {|element| element.capitalize!}
-      holiday_string = capital_holiday_string_array.join
+      holiday_string = capital_holiday_string_array.join('')
       output_holiday_string = "#{  holiday_string}: "
       output_supply = supply_array_value.join(", ")
       puts "  #{output_holiday_string + output_supply}"
